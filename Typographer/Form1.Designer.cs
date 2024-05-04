416f8c4726574f8385271abadf96ffc633712ba4
Дарья <@DESKTOP-BGVAOSK>
2024-05-04 23:45:06 +0300

    Добавьте файлы проекта.

---
+++ Typographer/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Typographer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChangeButton = new System.Windows.Forms.Button();
+            this.EnteredText = new System.Windows.Forms.TextBox();
+            this.ChangedText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ChangeButton
+            // 
+            this.ChangeButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ChangeButton.Location = new System.Drawing.Point(392, 154);
+            this.ChangeButton.Name = "ChangeButton";
+            this.ChangeButton.Size = new System.Drawing.Size(118, 55);
+            this.ChangeButton.TabIndex = 0;
+            this.ChangeButton.Text = "=>";
+            this.ChangeButton.UseVisualStyleBackColor = false;
+            this.ChangeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // EnteredText
+            // 
+            this.EnteredText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EnteredText.Location = new System.Drawing.Point(12, 12);
+            this.EnteredText.Multiline = true;
+            this.EnteredText.Name = "EnteredText";
+            this.EnteredText.Size = new System.Drawing.Size(357, 435);
+            this.EnteredText.TabIndex = 1;
+            // 
+            // ChangedText
+            // 
+            this.ChangedText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChangedText.Location = new System.Drawing.Point(528, 12);
+            this.ChangedText.Multiline = true;
+            this.ChangedText.Name = "ChangedText";
+            this.ChangedText.Size = new System.Drawing.Size(358, 435);
+            this.ChangedText.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(898, 450);
+            this.Controls.Add(this.ChangedText);
+            this.Controls.Add(this.EnteredText);
+            this.Controls.Add(this.ChangeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Типограф";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ChangeButton;
+        private System.Windows.Forms.TextBox EnteredText;
+        private System.Windows.Forms.TextBox ChangedText;
+    }
+}
+
---
+++ Typographer/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typographer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string RemoveExtraSpace(string EnteredText)
+        {
+            string result = Regex.Replace(EnteredText, @"\s+", " ");
+            return result;
+        }
+
+        private string HyphenWithoutSpace(string EnteredText)
+        {
+            string result = Regex.Replace(EnteredText, @"(?<! )-(?! )", "");
+            return result;
+        }
+
+        private string ReplacePlusMinus(string EnteredText)
+        {
+            string result = EnteredText.Replace("+/-", "±");
+            return result;
+        }
+
+        private string ReplaceTripleDots(string EnteredText)
+        {
+            string result = Regex.Replace(EnteredText, @"(?<!\.\.\.)(\.\.\.)", "...");
+            return result;
+        }
+
+        private string FormatArithmeticExpression(string EnteredText)
+        {
+            string result = EnteredText;
+            result = result.Replace("-", "–"); 
+            result = Regex.Replace(result, @"(\d+(?:\.\d+)?)\s*([-+*/])\s*(\d+(?:\.\d+)?)", "$1 $2 $3");
+            return result;
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            string final = RemoveExtraSpace(EnteredText.Text);
+            final = HyphenWithoutSpace(final);
+            final = ReplacePlusMinus(final);
+            final = ReplaceTripleDots(final);
+            final = FormatArithmeticExpression(final);
+            ChangedText.Text = final;
+        }
+    }
+}
